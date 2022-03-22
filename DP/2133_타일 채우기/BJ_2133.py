b664235ee2e00a9ebb12c61c4135e32cd6f97c38
dltmdrl1244 <dltmdrl1244@naver.com>
2022-03-22 20:46:55 +0900

    Create new file [2631_줄세우기]

--- DP/2133_타일 채우기/BJ_2133.py
+++ DP/2133_타일 채우기/BJ_2133.py
@@ -1,12 +1,10 @@
-n = int(input())
+n, k = map(int, input().split())
+c = [int(input()) for i in range(n)] # 코인의 종류
+dp = [0 for i in range(k+1)] # 사이즈 k+1만큼의 리스트 선언
+dp[0] = 1 # 인덱스 0은 동전을 1개만 쓸 때의 경우의 수를 고려하기 위해 선언
 
-dp = [0 for i in range(31)]
-dp[2] = 3
-
-for i in range(4, 31, 2):
-    dp[i] = dp[2] * dp[i - 2]
-    for j in range(4, i, 2):
-        dp[i] += 2 * dp[i - j]
-    dp[i] += 2
-    
-print(dp[n])
+for i in c:
+    for j in range(i, k+1):
+        if j-i >= 0:
+            dp[j] += dp[j-i]
+print(dp[k])
