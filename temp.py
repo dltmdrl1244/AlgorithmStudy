28363e78795f62b9bb4abcb9b5a452202f6bc44a
dltmdrl1244 <dltmdrl1244@naver.com>
2022-04-02 01:58:06 +0900

    Create new file[BJ_2294.py]

--- temp.py
+++ temp.py
@@ -1,18 +1,26 @@
 import sys
-n = int(sys.stdin.readline())
+n, k = map(int, sys.stdin.readline().split())
+coins = []
+dp = [-1 for _ in range(100001)]
 
-dp = [[0 for _ in range(10)] for _ in range(n+1)]
-
-dp[1] = [1 for _ in range(10)]
-dp[1][0] = 0
-
-for i in range(2, n+1) :
-    for j in range(10) :
-        if j == 0 :
-            dp[i][0] = dp[i-1][1]
-        elif j == 9 :
-            dp[i][9] = dp[i-1][8]
-        else :
-            dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
-
-print(sum(dp[n]) % 1000000000)
+for i in range(n) :
+    coins.append(int(sys.stdin.readline()))
+    
+for coin in coins :
+    dp[coin] = 1
+    
+for i in range(1, k+1) :
+    if dp[i] == 1 :
+        continue
+    arr = []
+    flag = 0
+    
+    for coin in coins :
+        if coin != 0 and i > coin :
+            if dp[i-coin] != -1 :
+                flag = 1
+                arr.append(dp[i-coin])
+    if flag :
+        dp[i] = min(arr) + 1
+    
+print(dp[k])
