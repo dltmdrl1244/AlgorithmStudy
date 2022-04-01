8a21136f0042de4237e23c776905182fdf3e4833
dltmdrl1244 <dltmdrl1244@naver.com>
2022-04-01 22:31:49 +0900

    Create new file[BJ_11057.py]

--- temp.py
+++ temp.py
@@ -1,11 +1,15 @@
 import sys
-
 n = int(sys.stdin.readline())
-arr = list(map(int, sys.stdin.readline().split()))
-dp = [0 for _ in range(n)]
-dp[0] = arr[0]
 
-for i in range(1, n) :
-    dp[i] = max(arr[i], dp[i-1] + arr[i])
+dp = [[0 for _ in range(10)] for _ in range(n+1)]
 
-print(max(dp))
+dp[1] = [1 for _ in range(10)]
+
+for i in range(2, n+1) :
+    for j in range(10) :
+        a = 0
+        for k in range(j+1) :
+            a += dp[i-1][k]
+        dp[i][j] = a
+
+print(sum(dp[n]) % 10007)
