38d42f7b93381c468bdc3bb6838887bb7836f7be
dltmdrl1244 <dltmdrl1244@naver.com>
2022-04-02 17:11:30 +0900

    Create new file[BJ_9465.py]

--- temp.py
+++ temp.py
@@ -1,26 +1,42 @@
 import sys
-n, k = map(int, sys.stdin.readline().split())
-coins = []
-dp = [-1 for _ in range(100001)]
+T = int(sys.stdin.readline())
 
-for i in range(n) :
-    coins.append(int(sys.stdin.readline()))
+def rotated(arr) :
+    n = len(arr)
+    m = len(arr[0])
+    result = [[0 for _ in range(n)] for _ in range(m)]
     
-for coin in coins :
-    dp[coin] = 1
+    for i in range(n) :
+        for j in range(m) :
+            result[j][n-i-1] = arr[i][j]
+            
+    return result
     
-for i in range(1, k+1) :
-    if dp[i] == 1 :
+    
+for _ in range(T) :
+    n = int(sys.stdin.readline())
+    dp = [[0, 0] for _ in range(100001)]
+    s = []
+    
+    for _ in range(2) :
+        s.append(list(map(int, sys.stdin.readline().split())))
+    s = rotated(s)
+    s.insert(0, [0, 0])
+    
+    dp[1] = s[1]
+    if n == 1 :
+        print(max(dp[1]))
         continue
-    arr = []
-    flag = 0
     
-    for coin in coins :
-        if coin != 0 and i > coin :
-            if dp[i-coin] != -1 :
-                flag = 1
-                arr.append(dp[i-coin])
-    if flag :
-        dp[i] = min(arr) + 1
+    dp[2][0] = dp[1][1] + s[2][0]
+    dp[2][1] = dp[1][0] + s[2][1]
+    if n == 2 :
+        print(max(dp[n]))
+        continue
     
-print(dp[k])
+    
+    for i in range(3, n+1) :
+        dp[i][0] = max(dp[i-1][1], dp[i-2][1]) + s[i][0]
+        dp[i][1] = max(dp[i-1][0], dp[i-2][0]) + s[i][1]
+    print(max(dp[i]))
+    
