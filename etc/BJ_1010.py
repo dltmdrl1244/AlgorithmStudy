501b96a7a1b77909e57edfb51df43c97809ebc87
dltmdrl1244 <dltmdrl1244@naver.com>
2022-04-12 00:52:25 +0900

    Solve a problem(BJ_1010.py)

---
+++ etc/BJ_1010.py
@@ -0,0 +1,16 @@
+import sys
+import operator as op
+from functools import reduce
+n = int(sys.stdin.readline())
+
+def nCr(n, r):
+    if n < 1 or r < 0 or n < r:
+        raise ValueError
+    r = min(r, n-r)
+    numerator = reduce(op.mul, range(n, n-r, -1), 1)
+    denominator = reduce(op.mul, range(1, r+1), 1)
+    return numerator // denominator
+
+for _ in range(n) :
+    n, m = map(int, sys.stdin.readline().split())
+    print(nCr(m, n))
