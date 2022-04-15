ec46051a1db0970e27a0a65b8217df1f0ef552ad
dltmdrl1244 <dltmdrl1244@naver.com>
2022-04-15 20:45:04 +0900

    Update & add a comment [1mod.py]

--- Class/Assignment_2/1mod.py
+++ Class/Assignment_2/1mod.py
@@ -8,31 +8,30 @@ coord_point = list(set(map(tuple, points)))
 def dist(a, b) :
     return (a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2 
     
-def solve(start, end, arr) :
-    # print("=== start solve(%d, %d)"%(start, end), arr, "start ===")
-    if start == end :
+def solve(N, arr) :
+    # 점 개수가 하나이면 거리 개념이 없으므로 무한대 리턴
+    if N == 1 :
         return [float('inf'), arr]
+    # 점 개수가 두 개이면 두 점을 y좌표 비교하여 정렬해서 리턴한다
+    # 두 점을 정렬할 때 시간 복잡도는 O(1)
+    elif N == 2 :
+        if arr[0][1] > arr[1][1] :
+            temp = arr[0]
+            arr[0] = arr[1]
+            arr[1] = temp
+        return [dist(arr[0], arr[1]), arr]
     
-    elif end - start == 1 :
-        if arr[start][1] > arr[end][1] :
-            temp = arr[start]
-            arr[start] = arr[end]
-            arr[end] = temp
-        return [dist(arr[start], arr[end]), arr]
+    # 가운데 점을 기준으로 자른다. (홀수 일때는 앞부분이 1 더 크다. ex: 7일 때 0~3, 4~6)
+    mid = N // 2
     
-    mid = (start + end) // 2
-    # print("mid : %d" %mid)
-    # print(arr[:mid + 1], arr[mid + 1:])
-    
-    left_d, left_arr = solve(0, len(arr[:mid+1]) - 1, arr[:mid + 1])
-    # print("left arr :", left_arr, "left_d :", left_d)
-    right_d, right_arr = solve(0, len(arr[mid+1:]) - 1, arr[mid + 1:])
-    # print("right arr :", right_arr, "right_d :", right_d)
-
+    # 양 쪽의 최소 거리를 구한다. 이 때 점의 개수는 
+    left_d, left_arr = solve(len(arr[:mid+1]), arr[:mid + 1])
+    right_d, right_arr = solve(len(arr[mid+1:]), arr[mid + 1:])
     d = min(left_d, right_d)
-
-    # d = min(solve(start, mid)[0], solve(mid+1, end)[0])
-    # print("d :", d)
+    # left_d, right_d 중 작은 값을 d에 저장. 이제 이 d을 가운데 경계 조사 케이스랑 비교한다.
+    
+    # left_arr과 right_arr은 길이가 N/2이고, y축좌표 기준으로 정렬되어 있다. 이를 병합한다.
+    # 병합하는 과정에서의 시간 복잡도는 O(n)
     i, j = 0, 0
     lena = len(left_arr)
     lenb = len(right_arr)
@@ -51,34 +50,31 @@ def solve(start, end, arr) :
     while j < lenb :
         new_arr.append(right_arr[j])
         j += 1
-    # print("(end)new_arr :", new_arr)
     
-    # print("mid :", arr[mid])
-    
+    # candidate 리스트는 경계선으로부터 x좌표가 d 이내인 점들의 리스트 (1차로 거르기)
     candidate = []
-    for i in range(start, end + 1) :
+    for i in range(N) :
         if (new_arr[i][0] - arr[mid][0]) ** 2 < d :
             candidate.append(new_arr[i])
     
-    # print("candidate :", candidate)
     
     can_len = len(candidate)
     for i in range(can_len - 1) :
         for j in range(i + 1, can_len) :
-            if i - j > 12 :
-                break
+            # candidate 리스트가 이미 y축 기준으로 정렬이 되어 있으므로, 한 번 d보다 멀리 떨어진 점이 등장하면
+            # 그 뒤로는 볼 필요가 없다. break
+            # 이 때, j for문은 최악의 경우에도 12번까지만 돌게 된다.
             if (candidate[i][1] - candidate[j][1]) ** 2 < d :
                 d = min(d, dist(candidate[i], candidate[j]))
             else :
                 break
-    # print("(end)d :", d)
-    # print("===end solve(%d, %d)"%(start, end), arr, " end===")
-
+            
+    # 1, 2, 3번 케이스를 모두 고려하였으므로 
     return [d, new_arr]
         
-
+# 중복을 제거한 리스트와, 처음 리스트의 개수가 다르면 중복, 즉 같은 좌표가 있다는 소리니까 0
 if len(points) != len(coord_point) :
     print("0")
 else :
     points = sorted(points, key = lambda x : x[0])
-    print(solve(0, n-1, points)[0])
+    print(solve(n, points)[0])
