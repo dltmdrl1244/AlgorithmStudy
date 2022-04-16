c638055e14c8d6e29c6ce3b9407c14131837ed03
dltmdrl1244 <dltmdrl1244@naver.com>
2022-04-17 02:03:14 +0900

    Add a comment [BJ_1197(prim).py]

--- Graph/BJ_1197(prim).py
+++ Graph/BJ_1197(prim).py
@@ -1,3 +1,6 @@
+# heapq 모듈 없이 2차원 리스트로 짜 보았는데 메모리 초과가 뜬다.
+# 정점 개수가 최대 10,000개 주어지는데 2차원 배열로 만들어서 메모리 초과가 뜨는 듯
+
 import sys
 v, e = map(int, sys.stdin.readline().split())
 costs = [[float('inf') for _ in range(v)] for _ in range(v)]
