1ec8a7dd85acaa0339f6f7525b69514fb0ddaad2
dltmdrl1244 <dltmdrl1244@naver.com>
2022-04-30 14:22:44 +0900

    Create new file(solved) [BJ_9663.py]

--- Back tracking/BJ_15650.py
+++ Back tracking/BJ_15650.py
@@ -6,6 +6,7 @@ s = []
 def f() :
     if len(s) == m :
         print(" ".join(map(str, s)))
+        return
     
     for i in range(1, n+1) :
         if i not in s and (len(s) == 0 or i > s[-1]) :
