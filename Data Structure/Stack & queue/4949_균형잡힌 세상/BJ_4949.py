ccbef8e56fb156f5dcb3aaf67f12c0b08061a402
dltmdrl1244 <dltmdrl1244@naver.com>
2022-05-07 16:17:26 +0900

    Create new file [BJ_4949.py]

--- Data Structure/Stack & queue/4949_균형잡힌 세상/BJ_4949.py
+++ Data Structure/Stack & queue/4949_균형잡힌 세상/BJ_4949.py
@@ -4,24 +4,24 @@ while True:
         break
     
     stack = []
-    temp = True
+    flag = True
     for i in s:
         if i == '(' or i == '[':
             stack.append(i)
         elif i == ')':
             if not stack or stack[-1] == '[':
-                temp = False
+                flag = False
                 break
             elif stack[-1] == '(':
                 stack.pop()
         elif i == ']':
             if not stack or stack[-1] == '(':
-                temp = False
+                flag = False
                 break
             elif stack[-1] == '[':
                 stack.pop()
                 
-    if temp == True and not stack:
+    if flag == True and not stack:
         print('yes')
     else:
         print('no')
