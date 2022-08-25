14ba7b2b7e0c425bed48cc5ff168028a089d2ed5
dltmdrl1244 <dltmdrl1244@naver.com>
2022-08-25 17:11:54 +0900

    1697.py, 코엔트

--- Graph/1697_숨바꼭질/BOJ_1697.py
+++ Graph/1697_숨바꼭질/BOJ_1697.py
@@ -1,9 +1,3 @@
-# 문제
-링크 : 
-해결여부 : 
-
-# 코드
-```
 import sys
 from collections import deque
 input = sys.stdin.readline
@@ -31,10 +25,5 @@ def bfs(n, k):
             q.append([cur*2, cnt+1])
             visited[cur*2] = True
 
+
 print(bfs(n, k))
-```
-
-# 문제 풀이
-간단한 bfs 문제인 것 같다. n-1, n+1, n*2를 모두 큐에 넣어가면서 하나씩 확인하되, 범위를 넘는지 확인하여 indexError가 발생하지 않게끔 해 준다.
-
-# 마무리
