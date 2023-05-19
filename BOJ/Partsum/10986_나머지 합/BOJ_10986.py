9fb82f0a7d6588a298045d8aeabfed1737e53f1c
dltmdrl1244 <dltmdrl1244@naver.com>
2023-05-19 12:43:30 +0900

    Create 10986 나머지 합

--- BOJ/Partsum/10986_나머지 합/BOJ_10986.py
+++ BOJ/Partsum/10986_나머지 합/BOJ_10986.py
@@ -1,12 +1,24 @@
 import sys
 input = sys.stdin.readline
-
+'''
+i~j 까지의 합 = s[j] - s[i-1]
+이것이 m의 배수이려면 (s[j] % m) == (s[i-1] % m)s
+'''
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 s = [0] * n
 t = 0
+rest = {0: 1}
+answer = 0
+
 for i in range(n):
     t += arr[i]
     s[i] = t
 
-print(s)
+    if (t % m) in rest:
+        answer += rest[t % m]
+        rest[t % m] += 1
+    else:
+        rest[t % m] = 1
+    
+print(answer)
