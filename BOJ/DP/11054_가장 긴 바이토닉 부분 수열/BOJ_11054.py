2b6e4b0acee1ed25fde50608f80c7a474ae8b0f2
dltmdrl1244 <dltmdrl1244@naver.com>
2024-02-18 18:07:51 +0900

    Create 11054 가장 긴 바이토닉 부분 수열

---
+++ BOJ/DP/11054_가장 긴 바이토닉 부분 수열/BOJ_11054.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+increase = [1 for _ in range(n)]
+decrease = [1 for _ in range(n)]
+
+for i in range(n):
+    for j in range(i):
+        if arr[i] > arr[j]:
+            increase[i] = max(increase[i], increase[j] + 1)
+
+for i in range(n-1, -1, -1):
+    for j in range(n-1, i, -1):
+        if arr[i] > arr[j]:
+            decrease[i] = max(decrease[i], decrease[j] + 1)
+
+print(max([increase[i] + decrease[i] for i in range(n)]) - 1)
